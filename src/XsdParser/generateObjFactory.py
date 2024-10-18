a79ac0bd6238e3df9f3a318cd4f96ad963a8720c
abandooon_ah <matong@mail.ustc.edu.cn>
2024-10-18 17:52:42 +0800

    修改objfactory的类型映射，因为其他版本的可能不在里面

--- src/XsdParser/generateObjFactory.py
+++ src/XsdParser/generateObjFactory.py
@@ -1,11 +1,11 @@
 import os
 
-from src.XsdParser.TypeMapping import mapXsdTypeToJava
+from src.XsdParser.TypeMapping import mapXsdTypeToJava, to_pascal_case
 
 
 def generate_object_factory(output_dir, package_name, mappings,objectFactoryTemplate):
     for map in mappings:
-        map['element_type']=mapXsdTypeToJava(map['element_name'], context='mixed')
+        map['element_type'] = mapXsdTypeToJava(map['element_name'], context='mixed') or to_pascal_case(map['element_name'])
         if map['complex_type'] == 'LParagraph' and map['element_name'] == 'FT':
             map['element_type'] = map['element_type'].replace("Overview", "")
 
